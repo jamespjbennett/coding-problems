bc73dbca7641e65cc7101832ab4268eafa92eb4d
jamespjbennett <jamespjbennett@hotmail.com>
2018-10-10 15:46:09 +0100

    serialize string for binary tree

--- google_tree_serializtion.rb
+++ google_tree_serializtion.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # This problem was asked by Google.
 #
 # Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
@@ -15,7 +17,34 @@
 # assert deserialize(serialize(node)).left.left.val == 'left.left'
 
 
-def serialize(root, string)
-  if root == ""
-
+def serialize(root, array)
+  if root == nil
+    array.push(nil)
+    return
+  else
+    array.push(root.val)
+    serialize(root.left, array)
+    serialize(root.right, array)
+  end
+  array.compact.join(",")
 end
+
+
+class Node
+  attr_reader :val, :left, :right
+  def initialize(val, left=nil, right=nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+node1 = Node.new('root', Node.new('l', Node.new('l.l')), Node.new('right'))
+array = []
+string = serialize(node1, array)
+p string
+#              "root"
+#           /          \
+#       "l"             "r"
+#     /     \         /     \
+# "l.l"    "l.r"    "r.l"   "r.r"
