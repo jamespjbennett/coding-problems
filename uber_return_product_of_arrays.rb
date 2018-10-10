14b57a56825605fbdc6da48b34af69e921289e6e
jamespjbennett <jamespjbennett@hotmail.com>
2018-10-10 12:11:10 +0100

    best i can do for now

--- uber_return_product_of_arrays.rb
+++ uber_return_product_of_arrays.rb
@@ -7,15 +7,6 @@ require 'pry'
 #
 # Follow-up: what if you can't use division?
 
-array =  (1..50000).to_a
-# expected_output = [120 (2*3*4*5), 60, (1*3*4*5), 40 (1*2*4*5), 30(1*2*3*5), 24 (1*2*3*4)]
-
-# Standard ruby. When thuis solution got to 10000 - it timed out. T
-# start = Time.now
-# array.map{|x| (array.select{|n| n!= x}).inject(:*)}
-# finish = Time.now
-# puts (finish - start).round(4)
-
 
 #  efficient algorithmic approach..
 
@@ -29,13 +20,13 @@ array =  (1..50000).to_a
 # [120,  60,   40,   30,   24]
 # resulting array - smaller the initial array value the larger its correponsindg result.
 
-start = Time.now
-sum = array.inject(:*)
-finish = Time.now
-p (finish - start).round(4)
-array.map{|v| sum/v}
-finish = Time.now
-p (finish - start).round(4)
+# start = Time.now
+# sum = array.inject(:*)
+# finish = Time.now
+# p (finish - start).round(4)
+# p array.sort.map{|v| sum/v}
+# finish = Time.now
+# p (finish - start).round(4)
 
 # for 100 results 0.0001
 # *38
@@ -43,3 +34,69 @@ p (finish - start).round(4)
 # * 111.7
 # for 10000 results 0.4248
 # for 100000 results this times out -
+
+
+
+
+# ANOTHER APPROACH:
+
+# FIGURED OUT THAT IF YOU SORT THE ARRAY FROM HIGH TO LOW AND DIVIDE CORRESPONDING I OF THE RESULTS - THE RESULTING NUMBER ALWAYS RETURN A COMPLETELY EVEN AND MIRRORED DOWN CURVE.
+
+# sum = array.inject(:*)
+# p array.sort.reverse
+# mapped =  array.sort.map{|v| sum/v}
+# p mapped
+#
+
+
+def product_with_double_iteration(a)
+  a.map{|x| (a.select{|n| n!= x}).inject(:*)}
+end
+
+def product_with_sum_and_division(a)
+  sum = a.inject(:*)
+  a = a.sort.reverse
+  a.each_with_index.map{|v,i| (v*(sum / (v.to_f*a[a.length-(i+1)])))}
+end
+
+array =  (1..1000).to_a
+# expected_output = [120 (2*3*4*5), 60, (1*3*4*5), 40 (1*2*4*5), 30(1*2*3*5), 24 (1*2*3*4)]
+
+# Standard ruby. When thuis solution got to 10000 - it timed out. T
+start = Time.now
+product_with_double_iteration(array)
+finish = Time.now
+puts (finish - start).round(4)
+
+start = Time.now
+product_with_sum_and_division(array)
+finish = Time.now
+p (finish - start).round(4)
+# for 100 results 0.0001
+# * 9
+# for 1000 results 0.0009
+# * 55
+# for 10000 results 0.0499
+# for 100000 results 4.8036
+# for 200000 results 23.1844
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
